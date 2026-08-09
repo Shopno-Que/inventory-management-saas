d993f474f0ecf224f1c81831e00e256034b24b1a
Osama Abdullah <abdullah21673@hotmail.com>
2026-08-09 16:01:43 +0600

    removed the profile table as supabase user table can store few additional data

--- db/schema/users.js
+++ db/schema/users.js
@@ -4,19 +4,3 @@ import { sql } from "drizzle-orm";
 export const supabaseAuthUser = pgTable("supabase_auth_user", {
     id: uuid("id").primaryKey(),
 });
-
-export const profiles = pgTable("profiles", {
-    id: uuid("id")
-        .primaryKey()
-        .references(() => supabaseAuthUser.id),
-
-    displayName: varchar("display_name"),
-
-    createdAt: timestamp("created_at", {
-        withTimezone: true,
-    }),
-
-    updatedAt: timestamp("updated_at", {
-        withTimezone: true,
-    }),
-});
