a5893e68f37a2a68eabd6f7492c8d72ff4892406
Osama Abdullah <abdullah21673@hotmail.com>
2026-08-23 17:25:03 +0600

    migrated to neo db cleanup

---
+++ drizzle/migrations/0008_brief_jubilee.sql
@@ -0,0 +1,11 @@
+DROP TABLE "saas_member_roles" CASCADE;--> statement-breakpoint
+DROP TABLE "saas_members" CASCADE;--> statement-breakpoint
+DROP TABLE "saas_permissions" CASCADE;--> statement-breakpoint
+DROP TABLE "saas_role_permissions" CASCADE;--> statement-breakpoint
+DROP TABLE "saas_roles" CASCADE;--> statement-breakpoint
+DROP TABLE "store_invitations" CASCADE;--> statement-breakpoint
+DROP TABLE "store_member_permissions" CASCADE;--> statement-breakpoint
+DROP TABLE "store_member_roles" CASCADE;--> statement-breakpoint
+DROP TABLE "store_permissions" CASCADE;--> statement-breakpoint
+DROP TABLE "store_role_permissions" CASCADE;--> statement-breakpoint
+DROP TABLE "store_roles" CASCADE;
